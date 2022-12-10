336fb0b1651031f9a66098c39747e6963e246dda
Janus2002 <swaglordtryhard@gmail.com>
2022-12-10 19:28:26 +0000

    Added multiple enemys to the game.

--- game/enemy.py
+++ game/enemy.py
@@ -2,10 +2,19 @@ import pygame
 import random as rd
 
 class Enemy:
-    eImg = pygame.image.load("Data/images/alien.png")
-    X = rd.randint(640,1100)
-    Y = rd.randint(0,654)
-    eRect = eImg.get_rect()
-    VY = 0.5
-    VX = 30
-    
+    eImg = []
+    X = []
+    Y = []
+    eRect = []
+    VY = []
+    VX = []
+
+    #To spawn multiple enemies
+    def enemies(self,n):
+        for i in range(n):
+            self.eImg.append(pygame.image.load("Data/images/alien.png"))
+            self.X.append(rd.randint(640,1100))
+            self.Y.append(rd.randint(0,654))
+            self.eRect.append(self.eImg[i].get_rect())
+            self.VY.append(1)
+            self.VX.append(40)
--- game/main.py
+++ game/main.py
@@ -21,10 +21,13 @@ def main():
 
     #Player
     player = Player()
+    pSpeed = 0.75
     score = 0
 
     #Enemy
     enemy = Enemy()
+    numOfEnemies = 7 
+    enemy.enemies(numOfEnemies)
 
     #Rocekt
     rocket = Rocket()
@@ -59,16 +62,16 @@ def main():
             #Checks if a button the keboard was pressed
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    player.VX += 0.75
+                    player.VX += pSpeed
 
                 if event.key == pygame.K_DOWN:
-                    player.VX -= 0.75
+                    player.VX -= pSpeed
 
                 if event.key == pygame.K_RIGHT:
-                    player.VY += 0.75
+                    player.VY += pSpeed
 
                 if event.key == pygame.K_LEFT:
-                    player.VY -= 0.75
+                    player.VY -= pSpeed
                     
                 if event.key == pygame.K_SPACE:
                     #Cannot fire spam
@@ -102,19 +105,27 @@ def main():
         player.pRect.y = player.Y
     
     #Enemy Movement
-        enemy.Y += enemy.VY
-        enemy.eRect.x = enemy.X
-        enemy.eRect.y = enemy.Y
+        for i in range(numOfEnemies):
+            enemy.Y[i] += enemy.VY[i]
+            enemy.eRect[i].x = enemy.X[i]
+            enemy.eRect[i].y = enemy.Y[i]
 
-        if enemy.Y < 0:
-            enemy.VY = 0.5
-            enemy.X -= enemy.VX
-        elif enemy.Y >= 654:
-            enemy.VY = -0.5
-            enemy.X -= enemy.VX
-        elif enemy.X <= 0:
-            enemy.VX = 0
-            enemy.VY = 0
+            if enemy.Y[i] < 0:
+                enemy.VY[i] = 1
+                enemy.X[i] -= enemy.VX[i]
+            elif enemy.Y[i] >= 654:
+                enemy.VY[i] = -1
+                enemy.X[i] -= enemy.VX[i]
+            
+            if rocket.rRect.colliderect(enemy.eRect[i]):
+                rocket.rstate = "ready"
+                score += 10
+                enemy.X[i] = rd.randint(640,1100)
+                enemy.Y[i] = rd.randint(0,654)
+                print(score)
+                rocket.rRect.x = -40
+                rocket.rRect.y = -40
+            SCREEN.blit(enemy.eImg[i], (enemy.X[i],enemy.Y[i]))
 
         #Rocket Movement
         if rocket.X >= 1148:
@@ -126,19 +137,8 @@ def main():
             rocket.X += rocket.VX
             rocket.rRect.x = rocket.X + 96
             rocket.rRect.y = rocket.Y + 34
-
-        #Collision 
-        if rocket.rRect.colliderect(enemy.eRect):
-            rocket.rstate = "ready"
-            score += 10
-            enemy.X = rd.randint(640,1100)
-            enemy.Y = rd.randint(0,654)
-            print(score)
-            rocket.rRect.x = -40
-            rocket.rRect.y = -40
             
         SCREEN.blit(player.pImg, (player.X,player.Y))
-        SCREEN.blit(enemy.eImg, (enemy.X,enemy.Y))
        
         #Update display when there is a change
         pygame.display.update()
